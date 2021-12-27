8a68fa271305c76ea4ec140dc0d7d4f0cad1ab44
takahiro_aoki <takahiro.aoki.0902@gmail.com>
2021-12-28 00:41:48 +0900

    add comment to code

--- modules/modeling.py
+++ modules/modeling.py
@@ -1,13 +1,24 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
-def modeling(dataFormodel: tuple, model: str = "LogisticRegression") -> tuple:
-    X_train, X_valid, y_train, y_valid = dataFormodel
+def modeling(dataForModel: tuple, model: str = "LogisticRegression") -> tuple:
+    """Returns trained model and its accuracy score
+    params:
+        dataForModel: tuple<pandas.DataFrame>
+            Its length is 4, and its elements are X_train, X_valid, y_train, y_valid in order.
+        model: str
+            The name of machine learning model. In this notebook, LogisticRegression only.
+            When unknown name are assigned, return None
+    return: tuple<(machine learning model), tuple<float>>
+        The first element is trained model.
+        The second element is dictionary which accuracy score of 'train' and 'valid' are in it.
+    """
+    X_train, X_valid, y_train, y_valid = dataForModel
 
     if (model == "LogisticRegression"):
         model = LogisticRegression()
     else:
-        return
+        return None
 
     model.fit(X_train, y_train)
     accuracy_train: float = accuracy_score(y_train, model.predict(X_train))
--- modules/prepare.py
+++ modules/prepare.py
@@ -1,4 +1,11 @@
 import pandas
 
 def getRawData(filePath: str) -> pandas.DataFrame:
+    """Returns raw dataset as pandas.DataFrame
+    params:
+        filePath: str
+            The absolute path to dataset.
+    return: pandas.DataFrame
+        Raw dataset
+    """
     return pandas.read_csv(filePath)
