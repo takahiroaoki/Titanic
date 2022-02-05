bded26e4d8d6d248f7e2db9bc8dd010f2e28ef1d
takahiro_aoki <takahiro.aoki.0902@gmail.com>
2022-02-06 00:54:23 +0900

    change variants' names

--- execute.sh
+++ execute.sh
@@ -5,24 +5,24 @@
 ## Execution of main.ipynb ##
 #############################
 # [Define Variants]
-OUTPUT_DIR="./output"
-FILE_NAME="test"
+OUTPUT_BASE="./output"
+DIR_NAME="test2"
 
 # Execution
-jupyter nbconvert main.ipynb --to markdown --output "${FILE_NAME}.md" --execute --debug
+jupyter nbconvert main.ipynb --to markdown --output "${DIR_NAME}.md" --execute --debug
 
 # Tuning path to display png files in markdown
-rm -rf "${OUTPUT_DIR}/${FILE_NAME}"
-mkdir "${OUTPUT_DIR}/${FILE_NAME}"
-mv -f "${FILE_NAME}.md" "${OUTPUT_DIR}/${FILE_NAME}"
-mv -f "${FILE_NAME}_files" "${OUTPUT_DIR}/${FILE_NAME}"
+rm -rf "${OUTPUT_BASE}/${DIR_NAME}"
+mkdir "${OUTPUT_BASE}/${DIR_NAME}"
+mv -f "${DIR_NAME}.md" "${OUTPUT_BASE}/${DIR_NAME}"
+mv -f "${DIR_NAME}_files" "${OUTPUT_BASE}/${DIR_NAME}"
 
 #################
 ## LINE notify ##
 #################
 # [Define Variants]
 MY_TOKEN="LINE NOTIFY TOKEN"
-MESSAGE="We executed main.ipynb and created ${OUTPUT_DIR}/${FILE_NAME}/${FILE_NAME}.md."
+MESSAGE="We executed main.ipynb and created ${OUTPUT_BASE}/${DIR_NAME}/${DIR_NAME}.md."
 
 # Notification
 curl -X POST -H "Authorization: Bearer ${MY_TOKEN}" -F "message=${MESSAGE}" https://notify-api.line.me/api/notify
